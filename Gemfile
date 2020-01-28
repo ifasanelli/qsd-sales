0f8d6f6e3ad73345d4103017628404ebc1c64efb
Joao Alberto e Joao Victor <par05@campuscode.com.br>
2020-01-28 16:20:09 -0300

    Criação de Models e Home index

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
+gem 'devise'
 gem 'jbuilder', '~> 2.7'
 gem 'puma', '~> 4.1'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
---
+++ spec/factories/costumers.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :costumer do
+    name { "MyString" }
+    address { "MyString" }
+    document { "MyString" }
+    email { "MyString" }
+    phone { "MyString" }
+    birth_date { "2020-01-28" }
+  end
+end
