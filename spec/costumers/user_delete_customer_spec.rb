8497a2b86daeba8b17157a6f04524d3401794991
Joao Alberto <jooalberto1@gmail.com>
2020-02-07 00:55:15 -0300

    Estilização com Locastyle

--- spec/costumers/user_delete_customer_spec.rb
+++ spec/costumers/user_delete_customer_spec.rb
@@ -11,8 +11,29 @@ feature 'User delete customer' do
     # Act
     visit root_path
     click_on 'Clientes'
-    click_on customer.name.to_s
+    within("tr#customer-#{customer.id}") do
+      find('.ls-ico-zoomin').click
+    end
     click_on 'Apagar'
+    # Assert
+    expect(current_path).to eq(customers_path)
+    expect(page).not_to have_content(customer.name)
+    expect(page).to have_content(other_customer.name)
+  end
+
+  scenario 'by index' do
+    # Arrange
+    customer = create(:customer)
+    other_customer = create(:customer, name: 'Edgar A Poe',
+                                       email: 'edgar@gmail.com',
+                                       document: '455.725.668-01',
+                                       phone: '(12) 98216-2277')
+    # Act
+    visit root_path
+    click_on 'Clientes'
+    within("tr#customer-#{customer.id}") do
+      find('.ls-ico-remove').click
+    end
     # Assert
     expect(current_path).to eq(customers_path)
     expect(page).not_to have_content(customer.name)
--- spec/costumers/user_edit_customer_spec.rb
+++ spec/costumers/user_edit_customer_spec.rb
@@ -7,8 +7,28 @@ feature 'User edit customer' do
     # Act
     visit root_path
     click_on 'Clientes'
-    click_on customer.name.to_s
+    within("tr#customer-#{customer.id}") do
+      find('.ls-ico-zoomin').click
+    end
     click_on 'Editar'
+    fill_in 'Nome', with: 'George R R Martin'
+    click_on 'Salvar'
+    # Assert
+    expect(page).not_to have_content(customer.name)
+    expect(page).to have_content('George R R Martin')
+    expect(page).to have_content(customer.email)
+    expect(page).to have_content(customer.phone)
+  end
+
+  scenario 'by index' do
+    # Arrange
+    customer = create(:customer)
+    # Act
+    visit root_path
+    click_on 'Clientes'
+    within("tr#customer-#{customer.id}") do
+      find('.ls-ico-pencil').click
+    end
     fill_in 'Nome', with: 'George R R Martin'
     click_on 'Salvar'
     # Assert
@@ -23,7 +43,9 @@ feature 'User edit customer' do
     customer = create(:customer)
     # Act
     visit customers_path
-    click_on customer.name.to_s
+    within("tr#customer-#{customer.id}") do
+      find('.ls-ico-zoomin').click
+    end
     click_on 'Editar'
     fill_in 'Nome', with: ''
     click_on 'Salvar'
