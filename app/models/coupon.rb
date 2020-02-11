8761aea4dd82adbd77fd810a5c2a69c5d5f955dc
João Victor <victorpow@live.com>
2020-02-11 12:34:23 -0200

    Iniciando mock

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -6,8 +6,18 @@ class Coupon
   end
 
   def self.all
-    [new(name: 'NATLOCA01', discount: 21),
-     new(name: 'NATLOCA02', discount: 21)]
+    response = Faraday.get('http://localhost:3000/api/v1/coupon')
+
+    return [] if response.status == 500
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    coupon_list = []
+    json.each do |item|
+      coupon_list << Coupon.new(item[:name], item[:discount])
+    end
+
+    coupon_list
   end
 
   def self.find(name)
