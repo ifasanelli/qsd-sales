6c3cc795225ede64b2241479552de7f9d64f48cc
Marina e Victor <par06@campuscode.com.br>
2020-02-11 16:49:26 -0300

    remove useless scenario from finish order spec

--- spec/features/orders/admin_finish_order_spec.rb
+++ spec/features/orders/admin_finish_order_spec.rb
@@ -22,20 +22,4 @@ feature 'User finish order' do
     expect(page).to have_content('Status: Aprovado')
     expect(page).not_to have_link('Aprovar')
   end
-
-  xscenario 'and coupon must be valid' do
-    # Mock do cupom
-    user = create(:user)
-    customer = create(:customer)
-    order = create(:order, user: user, customer: customer, coupon: coupon.id)
-
-    visit root_path
-    click_on 'Pedidos'
-    click_on order.code.to_s
-    click_on 'Aprovar'
-
-    expect(page).to have_content('Cupom inválido')
-    expect(page).to have_content(order.code.to_s)
-    expect(page).to have_content('Status: Aberto')
-  end
 end
