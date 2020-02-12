15b69c8fbb918276652a82465d3d6529645a5048
Joao Alberto <jooalberto1@gmail.com>
2020-02-12 00:16:33 -0300

    Coupon mock feito

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,13 +1,37 @@
 class Coupon
-  attr_reader :name, :discount
-  def initialize(name: '', discount: 0)
+  attr_reader :name, :discount, :status
+  def initialize(name: '', discount: 0, status: '')
     @name = name
     @discount = discount
+    @status = status
   end
 
   def self.all
     [new(name: 'NATLOCA01', discount: 21),
      new(name: 'NATLOCA02', discount: 21)]
+  end
+
+  def self.api_version
+    'v1'
+  end
+
+  def self.endpoint
+    Rails.configuration.qsd_apis[:coupon_url]
+  end
+
+  def self.product_url
+    "#{endpoint}/api/#{api_version}"
+  end
+
+  def self.burn(code)
+    request_url = "#{product_url}/coupon/#{code}/burn"
+    response = Faraday.get(request_url)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    return [] if response.status == 404
+
+    @status = json[:status]
   end
 
   def self.find(name)
